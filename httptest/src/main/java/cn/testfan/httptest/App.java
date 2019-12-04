de53b6583874795ea52eaaffbf1eb4116b6fba62
DESKTOP-V4QMOST\xiang <1533646516@qq.com>
2019-12-04 20:56:21 +0800

    测试代码提交

---
+++ httptest/src/main/java/cn/testfan/httptest/App.java
@@ -0,0 +1,14 @@
+package cn.testfan.httptest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+        System.out.println( "337+15+24.8" );
+    }   
+}
